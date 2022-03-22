c08f58104b88b2e8ea54a796ab04e0937a837fd8
Paolo Di Martino <paolo.dimartino@outlook.it>
2022-03-22 17:45:04 +0000

    Completed javascript-data-model exercise.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,57 @@
+var orderHistory = [{
+  orderDate: '04/08/2020',
+  totalPrice: 34.00,
+  deliveryAddress: 'JS Masher',
+  orderId: '114-3941689-8772232',
+  deliveryDate: '08/08/2020',
+  items: [{
+    title: 'JavaScript for impatient programmers',
+    author: 'Rauschmayer, Dr. Axel',
+    returnWindow: '07/09/2020',
+    price: 31.55
+  }]
+}, {
+  orderDate: '19/07/2020',
+  totalPrice: 44.53,
+  deliveryAddress: 'JS Masher',
+  orderId: '113-9984268-1280257',
+  deliveryDate: '20/07/2020',
+  items: [{
+    title: 'The Timeless Way of Building',
+    author: 'Alexander, Christopher',
+    returnWindow: '19/08/2020',
+    price: 41.33
+  }]
+}, {
+  orderDate: '04/07/2020',
+  totalPrice: 17.22,
+  deliveryAddress: 'JS Masher',
+  orderId: '114-2875557-9059409',
+  deliveryDate: '07/07/2020',
+  items: [{
+    title: 'Gamecube Controller Adapter. No Driver and No Lag-Gamecube Adapter',
+    returnWindow: '05/08/2020',
+    price: 15.98
+  }]
+}, {
+  orderDate: '03/07/2020',
+  totalPrice: 138.93,
+  deliveryAddress: 'JS Masher',
+  orderId: '113-2883177-2648248',
+  deliveryDate: '05/07/2020',
+  items: [{
+    title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+    returnWindow: '04/08/2020',
+    price: 94.95
+  },
+  {
+    title: 'The Art of Sql',
+    author: 'Faroult, Stephane',
+    returnWindow: '04/08/2020',
+    price: 33.99
+  }]
+}
+
+];
+
+console.log('orderHistory:', orderHistory);
