453be8bb7be90d3b10c0d6607934aa1820025a19
Paolo Di Martino <paolo.dimartino@outlook.it>
2022-03-28 17:22:21 +0000

    Completed dom-querying exercise.

---
+++ dom-querying/main.js
@@ -0,0 +1,16 @@
+console.log('hello, world');
+
+var h1Element = document.querySelector('h1');
+console.log(h1Element);
+console.dir(h1Element);
+
+var idEl = document.querySelector('#explanation');
+console.log(idEl);
+console.dir(idEl);
+
+var hintEl = document.querySelector('.hint');
+console.log(hintEl);
+console.dir(hintEl);
+
+var exampleLinkEl = document.querySelectorAll('.example-link');
+console.log(exampleLinkEl);
