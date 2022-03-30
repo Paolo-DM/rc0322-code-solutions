bad9a1dd348696cf2cd383804e262d6604b7e320
Paolo Di Martino <paolo.dimartino@outlook.it>
2022-03-30 18:11:12 +0000

    Completed the second half of the javascript-forms exercise.

---
+++ javascript-forms/form-values.js
@@ -0,0 +1,12 @@
+var $contactForm = document.querySelector('#contact-form');
+
+$contactForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var formValues = {};
+  formValues.name = $contactForm.elements.name.value;
+  formValues.email = $contactForm.elements.email.value;
+  formValues.message = $contactForm.elements.message.value;
+
+  console.log(formValues);
+  $contactForm.reset();
+});
