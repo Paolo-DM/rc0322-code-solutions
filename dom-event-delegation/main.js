c7bd653ab26ca6e266af144580b7cda4cd352d6f
Paolo Di Martino <paolo.dimartino@outlook.it>
2022-03-31 18:12:28 +0000

    Added the if statement to remove only the button elements.

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -2,7 +2,11 @@ var $taskList = document.querySelector('.task-list');
 $taskList.addEventListener('click', function (event) {
   console.log('event.target', event.target);
   console.log('event.target.tagName', event.target.tagName);
-  var $closest = event.target.closest('.task-list-item');
-  console.log('closest .task-list-item', $closest);
-  $closest.remove();
+
+  if (event.target.tagName === 'BUTTON') {
+    var $closest = event.target.closest('.task-list-item');
+    console.log('closest .task-list-item', $closest);
+    $closest.remove();
+  }
+
 });
