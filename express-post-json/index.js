586429ed6fb34fe78a176ab43ae70374c74e91f1
Paolo Di Martino <paolo.dimartino@outlook.it>
2022-05-09 23:01:48 +0000

    Completed express-post-json exercise.

---
+++ express-post-json/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+let nextId = 1;
+const grades = {};
+
+app.get('/api/grades', (req, res) => {
+  const gradesArray = [];
+  for (const key in grades) {
+    gradesArray.push(grades[key]);
+  }
+  res.json(gradesArray);
+});
+
+app.use(express.json());
+
+app.post('/api/grades', (req, res) => {
+  const grade = {};
+  grade.id = nextId;
+  grade.name = req.body.name;
+  grade.course = req.body.course;
+  grade.score = req.body.score;
+  grades[nextId] = grade;
+  nextId++;
+  res.status(201).end();
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening on port 3000!');
+});
