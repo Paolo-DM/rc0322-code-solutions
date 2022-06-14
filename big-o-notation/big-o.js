cfd81e0d2a597470e0d355bf5560f6f023b561e7
Paolo Di Martino <paolo.dimartino@outlook.it>
2022-06-14 00:09:58 +0000

    Fixed

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -24,20 +24,20 @@ function uniqueQuadratic(words) {
     i < words.length;               // 2 * n = O(2n) ~= O(n)
     i++                             // 2 * n = O(2n) ~= O(n)
   ) {
-    const word = words[i];          // 1 * n = O(n)
-    let isUnique = true;            // 1 * 1 = O(1)
+    const word = words[i];          // 2 * n = O(2n) ~= O(n)
+    let isUnique = true;            // 1 * n = O(n)
     for (
-      let c = 0;                    // 1 * 1 = O(1)
-      c < i;                        // 1 * 1 = O(1)
-      c++                           // 2 * n = O(2n) ~= O(n)
+      let c = 0;                    // 1 * n = O(n)
+      c < i;                        // 1 * n^2 = O(n^2)
+      c++                           // 2 * n^2 = O(2n^2) ~= O(n^2)
     ) {
       const comparing = words[c];   // 2 * n^2 = O(2n^2) ~= O(n^2)
       if (comparing === word) {     // 1 * n^2 = O(n^2)
         isUnique = false;           // 1 * n^2 = O(n^2)
       }
     }
-    if (isUnique) {                 // 1 * 1 = O(1)
-      unique[unique.length] = word; // 3 * n = O(3n) ~= O(n)
+    if (isUnique) {                 // 1 * n = O(n)
+      unique[unique.length] = word; // 2 * n = O(2n) ~= O(n)
     }
   }
   return unique;                    // 1 * 1 = O(1)
